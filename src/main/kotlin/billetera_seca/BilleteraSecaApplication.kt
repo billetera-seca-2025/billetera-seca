392dffc97448afc987fdb1f95ff06abaaec9b71d
maxim <maximiliano.maglio@ing.austral.edu.ar>
2025-04-10 16:06:56 -0300

    first commit

---
+++ src/main/kotlin/billetera_seca/BilleteraSecaApplication.kt
@@ -0,0 +1,11 @@
+package billetera_seca
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BilleteraSecaApplication
+
+fun main(args: Array<String>) {
+    runApplication<BilleteraSecaApplication>(*args)
+}
---
+++ src/test/kotlin/billetera_seca/BilleteraSecaApplicationTests.kt
@@ -0,0 +1,13 @@
+package billetera_seca
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BilleteraSecaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
